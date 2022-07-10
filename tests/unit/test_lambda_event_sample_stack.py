0af2918905db0d169a4c72092991ad4a4802d0be
tsuji-tomonori <160441107@ccalumni.meijo-u.ac.jp>
2022-07-10 22:54:03 +0900

    Initial commit

---
+++ tests/unit/test_lambda_event_sample_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from lambda_event_sample.lambda_event_sample_stack import LambdaEventSampleStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in lambda_event_sample/lambda_event_sample_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = LambdaEventSampleStack(app, "lambda-event-sample")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
